f456324a3a48ad6208986c07247066aa3a534aab
aarav-g-168 <guptaaarav987@gmail.com>
2026-03-27 22:25:31 +0530

    updated the tasks for web

--- lib/data.ts
+++ lib/data.ts
@@ -14,7 +14,7 @@ export const domainToName: { [key in Domain]: string } = {
 
 export const domainToTaskLink: { [key in Domain]: string } = {
   web: "https://drive.google.com/file/d/1D6RC-jpeYapzXOsHEW7fX22KHe5ToAD6/preview",
-  ios: "https://docs.google.com/document/d/1WKBRFT09UdXiT94ZrbDYJA6sglgLtWZySeqNfWlF-5g/edit?usp=sharing",
+  ios: "https://docs.google.com/document/d/1WKBRFT09UdXiT94ZrbDYJA6sglgLtWZySeqNfWlF-5g/preview",
   android:
     "https://drive.google.com/file/d/1ktsRf5idRkZ2kOl1IE-j2fb9tEbRRjuc/preview",
   ml: "https://drive.google.com/file/d/1zcDNqCjSqMdzN_WVW8aNKRG9xMWPNAJ8/preview",
