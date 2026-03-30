4a050c48459da352121c38d1e8719679361b4f9d
aarav-g-168 <guptaaarav987@gmail.com>
2026-03-30 11:37:43 +0530

    added mailto option in the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,14 +4,24 @@ import { Box, Flex, Text } from "@chakra-ui/react"
 const Footer = () => {
   return (
     <footer>
-  <Box height={10} bg="black" color="white" p={4}  fontSize={["sm", "md"]} fontWeight="600">
-    <Flex justifyContent="space-around" alignItems="center" height="100%">
-      
-      
-      <Text>For any queries, kindly mail us at ios@vit.ac.in</Text>
-    </Flex>
-  </Box>
-</footer>
+      <Box height={10} bg="black" color="white" p={4} fontSize={["sm", "md"]} fontWeight="600">
+        <Flex justifyContent="space-around" alignItems="center" height="100%">
+
+          <Text>
+            For any queries, kindly mail us at{" "}
+            <Box
+              as="a"
+              href="mailto:ios@vit.ac.in?subject=Query regarding ADG VIT"
+              color="blue.300"
+              _hover={{ textDecoration: "underline", color: "blue.400" }}
+            >
+              ios@vit.ac.in
+            </Box>
+          </Text>
+          
+        </Flex>
+      </Box>
+    </footer>
 
   )
 }
